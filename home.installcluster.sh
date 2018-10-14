4bb7568247bc325bcaaff94fc4261519b47fd7dd
Ferdinando Simonetti <ferdinando.simonetti@outlook.com>
2018-10-14 23:21:24 +0200

    added home installcluster script

---
+++ home.installcluster.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+# Ambari host
+export AMBARISRVHOST="l01.lan"
+# Ambari cluster
+export CLUSTERNAME="home"
+export BPNAME="home"
+export CURLCMD="curl -k -u admin:admin -i"
+export BASEURL="http://${AMBARISRVHOST}:8080/api/v1"
+export BASECLU="${BASEURL}/clusters/${CLUSTERNAME}"
+export BASEVDF="${BASEURL}/version_definitions"
+export BASEBP="${BASEURL}/blueprints"
+
+# add Hadoop version local repository to Ambari
+${CURLCMD} -H "X-Requested-By:ambari" -X POST ${BASEVDF} -d @version-definitions.json
+# add blueprint information to Ambari
+${CURLCMD} -H "X-Requested-By:ambari" -X POST ${BASEBP}/${BPNAME} -d @${BPNAME}.cluster-config.json
+# apply blueprint to host topology
+${CURLCMD} -H "X-Requested-By:ambari" -X POST ${BASECLU} -d @${BPNAME}.host-mapping.json

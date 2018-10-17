321b771bf999f53fa09d7a1ad28172c871f9c819
Ferdinando Simonetti <ferdinando.simonetti@outlook.com>
2018-10-17 21:43:27 +0200

    home config, cleanup

--- home.installcluster.sh
+++ home.installcluster.sh
@@ -3,7 +3,7 @@
 export AMBARISRVHOST="l01.lan"
 # Ambari cluster
 export CLUSTERNAME="home"
-export BPNAME="home2"
+export BPNAME="home"
 export CURLCMD="curl -k -u admin:admin -i"
 export BASEURL="http://${AMBARISRVHOST}:8080/api/v1"
 export BASECLU="${BASEURL}/clusters/${CLUSTERNAME}"
@@ -11,7 +11,7 @@ export BASEVDF="${BASEURL}/version_definitions"
 export BASEBP="${BASEURL}/blueprints"
 
 # add Hadoop version local repository to Ambari
-#${CURLCMD} -H "X-Requested-By:ambari" -X POST ${BASEVDF} -d @${BPNAME}.version-definitions.json
+${CURLCMD} -H "X-Requested-By:ambari" -X POST ${BASEVDF} -d @${BPNAME}.version-definitions.json
 # add blueprint information to Ambari
 ${CURLCMD} -H "X-Requested-By:ambari" -X POST ${BASEBP}/${BPNAME} -d @${BPNAME}.cluster-config.json
 # apply blueprint to host topology
